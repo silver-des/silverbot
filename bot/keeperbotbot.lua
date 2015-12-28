a330f6faa01cb8f93f0b9edbc5a3b588ac3d6af5
m2132 <matin_igdery@yahoo.com>
2015-12-28 19:06:07 +0330

    Update keeperbotbot.lua

--- bot/keeperbotbot.lua
+++ bot/keeperbotbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {118333567,121259485,70742427,0,tonumber(our_id)},--Sudo users
+    sudo_users = {121122782,68143146,102952420,158987991,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
